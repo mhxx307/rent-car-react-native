73efcd4a0c29133ede502097694fd4932b274088
longle23 <longle2305@gmail.com>
2022-11-18 21:10:43 +0700

    #20 finish SignUp screen
    longle have been finished coding layout for SignUp, he worked so hard to make this work.

---
+++ src/screens/SignUp/styles.js
@@ -0,0 +1,31 @@
+import COLORS from '~/components/Colors';
+
+const { StyleSheet, StatusBar } = require('react-native');
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+        padding: 20,
+    },
+    login: {
+        fontSize: 16,
+        color: COLORS.white,
+        alignSelf: 'center',
+    },
+    forgot: {
+        alignSelf: 'center',
+        color: COLORS.textColor,
+        marginTop: 10,
+    },
+    policy: {
+        marginTop: 30,
+        marginLeft: 5,
+        flexDirection: 'row'
+    },
+    checkbox: {
+        borderRadius: 6,
+    }
+});
+
+export default styles;
--- src/screens/index.js
+++ src/screens/index.js
@@ -3,3 +3,4 @@ export { default as CategoriesScreen } from './Categories';
 export { default as SavedScreen } from './Saved';
 export { default as MyRentScreen } from './MyRent';
 export { default as LoginScreen } from './Login';
+export { default as SignUpScreen } from './SignUp';
